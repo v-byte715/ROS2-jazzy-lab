1ab9704d5f16bdcccf6efb2b67d0580e38002eb0
v-byte715 <vasanthvigneshwaranm@gmail.com>
2026-05-18 20:52:12 +0530

    Implement MinimalPublisher for ROS2 message publishing

---
+++ exp2_custom_pubsub/src/py_pubsub/py_pubsub/publisher_member_function.py
@@ -0,0 +1,27 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+
+class MinimalPublisher(Node):
+
+    def __init__(self):
+        super().__init__('minimal_publisher')
+        self.publisher_ = self.create_publisher(String, 'chatter', 10)
+        self.timer = self.create_timer(1.0, self.timer_callback)
+        self.count = 0
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = f'Hello ROS2: {self.count}'
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.count += 1
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MinimalPublisher()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
