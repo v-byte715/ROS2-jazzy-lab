dadd916ecc895fc256f96c345e2124ec63a6888e
v-byte715 <vasanthvigneshwaranm@gmail.com>
2026-05-18 20:55:45 +0530

    Update setup.py

--- exp2_custom_pubsub/src/py_pubsub/setup.py
+++ exp2_custom_pubsub/src/py_pubsub/setup.py
@@ -25,8 +25,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'talker = py_pubsub.publisher_member_function:main',
-            'listener = py_pubsub.subscriber_member_function:main',
+            'publisher_member_function = py_pubsub.publisher_member_function:main',
         ],
     },
 )
